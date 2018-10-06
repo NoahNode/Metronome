05299963c616e23bd1d1c5ee127116675421348f
Noah Irwin <Noah@Noahs-MBP.lan>
2018-10-06 19:12:08 +0100

    Added metronome functionality

--- src/Metronome.js
+++ src/Metronome.js
@@ -3,9 +3,7 @@ import './Metronome.css'
 import click1 from './clicks/click1.wav';
 import click2 from './clicks/click2.wav';
 
-
 class Metronome extends Component{
-
     constructor(props){
         super(props);
 
@@ -22,13 +20,43 @@ class Metronome extends Component{
 
     handleBpmChange = event => {
         const bpm = event.target.value;
-        this.setState({bpm})
+
+        if(this.state.playing){
+            clearInterval(this.timer)
+            this.timer = setInterval(this.playClick, (60/bpm)*1000)
+            this.setState({count:0, bpm})
+        } else{
+            this.setState({bpm})
+        }
+    }
+
+    playClick = () => {
+        const{count, beatsPerMeasure} = this.state;
+
+        if((count % beatsPerMeasure) === 0){
+            this.click2.play()
+        } else{
+            this.click1.play()
+        }
+
+        this.setState( state => ({
+            count: (state.count + 1) % beatsPerMeasure
+        }));
     }
 
     startStop = () => {
-        this.click1.play();
+        if(this.state.playing){
+            clearInterval(this.timer)
+            this.setState({playing: false})
+        }else{
+            this.timer = setInterval(this.playClick, (60/this.state.bpm)*1000)
+            this.setState({
+                count: 0,
+                playing: true,
+            },
+            this.playClick);
+        }
     }
-
 
     render(){
         const { playing, bpm } = this.state;
@@ -36,7 +64,7 @@ class Metronome extends Component{
         return(
             <div className="metronome">
                 <div className="bpmSlider">
-                    <input type="range" min="60" max="240" value={bpm} onChange={this.handleBpmChange} />
+                    <input type="range" min="30" max="300" value={bpm} onChange={this.handleBpmChange} />
                 </div>
                 <button onClick={this.startStop}>{playing ? 'Stop' : 'Start'}</button>
             </div>
